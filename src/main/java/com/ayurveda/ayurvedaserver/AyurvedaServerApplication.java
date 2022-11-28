7893e3dc39c60b325c5a59514d0a8ba2a3e2339c
Rajat Mahesh Gupta <guptarajat978@gmail.com>
2022-11-27 21:38:56 -0800

    inital commit

---
+++ src/main/java/com/ayurveda/ayurvedaserver/AyurvedaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ayurveda.ayurvedaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyurvedaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyurvedaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayurveda/ayurvedaserver/AyurvedaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayurveda.ayurvedaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyurvedaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
